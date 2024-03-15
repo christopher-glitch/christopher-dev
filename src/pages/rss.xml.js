0dd0a5ab207c6ab5c5945ad9945975b1732d9b1d
christopher-glitch <christph.glitch@gmail.com>
2024-03-16 05:37:48 +0900

    rssフィードについて修正

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { getEntryResponse } from '../lib/client';
 
 export async function GET(context) {
-	const posts = await getCollection('entry');
+	const posts = (await getEntryResponse()).contents;
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/entry/${post.slug}/`,
+			...post,
+			link: `/entry/${post.id}/`,
 		})),
 	});
 }
